c1e9d07bade50b73776844e256dcae8277f44d3d
Lazurite <lazurite@tom.com>
2022-07-19 21:49:17 +0800

    fix bug.

--- tools/train.py
+++ tools/train.py
@@ -33,25 +33,11 @@ from xgboost import XGBRegressor
 sys.path.insert(0, os.getcwd())
 sys.path.insert(0, os.path.dirname(os.getcwd()))
 from dataset.dataset import Dataset
-
+from averageing_model import AveragingModels
 warnings.filterwarnings('ignore')
 
 
-class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
-    def __init__(self, models):
-        self.models = models
 
-    def fit(self, X, y):
-        self.models_ = [clone(x) for x in self.models]
-        for model in self.models_:
-            model.fit(X, y)
-        return self
-
-    def predict(self, X):
-        predictions = np.column_stack([
-            model.predict(X) for model in self.models_
-        ])
-        return np.mean(predictions, axis=1)
 
 
 class Train:
