d6ea9aa7dfde79285d64d7521a10c99392dff133
Lazurite <lazurite@tom.com>
2022-07-19 21:50:02 +0800

    add averageing_model.py

---
+++ tools/averageing_model.py
@@ -0,0 +1,18 @@
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+import numpy as np
+
+class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, models):
+        self.models = models
+
+    def fit(self, X, y):
+        self.models_ = [clone(x) for x in self.models]
+        for model in self.models_:
+            model.fit(X, y)
+        return self
+
+    def predict(self, X):
+        predictions = np.column_stack([
+            model.predict(X) for model in self.models_
+        ])
+        return np.mean(predictions, axis=1)
